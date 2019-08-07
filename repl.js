dad523fa71c24465f6808ebf6b4bd2ec0348b063
Donald Ball <donald.ball@gmail.com>
2019-08-06 21:22:28 -0400

    Reformat with 2 space indents

--- repl.js
+++ repl.js
@@ -2,199 +2,199 @@
 
 const symbol_re = "[a-zA-Z\-_?!*+/][a-zA-Z\-_?!*+/0-9']*";
 
-const token_types =
-      [["LPAREN", "\\("],
-       ["RPAREN", "\\)"],
-       ["WHITESPACE", "\\s+"],
-       ["INTEGER", "-?\\d+"],
-       ["BOOLEAN", "true|false"],
-       ["NIL", "nil"],
-       ["KEYWORD", ":" + symbol_re],
-       ["SYMBOL", symbol_re],
-      ].map(([name, re]) => ({ name: name, re: new RegExp("^" + re)}));
+const token_types = [
+  ["LPAREN", "\\("],
+  ["RPAREN", "\\)"],
+  ["WHITESPACE", "\\s+"],
+  ["INTEGER", "-?\\d+"],
+  ["BOOLEAN", "true|false"],
+  ["NIL", "nil"],
+  ["KEYWORD", ":" + symbol_re],
+  ["SYMBOL", symbol_re],
+].map(([name, re]) => ({ name: name, re: new RegExp("^" + re)}));
 
 const tokenize = (s) => {
-    let results = [];
-    let offset = 0;
-    while (offset < s.length) {
-	let matched = false;
-	for (const {name, re} of token_types) {
-	    const [match] = re.exec(s.substr(offset)) || [];
-	    if (match != null) {
-		results.push([name, match]);
-		offset = offset + match.length;
-		matched = true;
-		break;
-	    }
-	}
-	if (!matched) {
-	    throw { msg: "Invalid syntax", offset: offset, s: s };
-	}
+  let results = [];
+  let offset = 0;
+  while (offset < s.length) {
+    let matched = false;
+    for (const {name, re} of token_types) {
+      const [match] = re.exec(s.substr(offset)) || [];
+      if (match != null) {
+        results.push([name, match]);
+        offset = offset + match.length;
+        matched = true;
+        break;
+      }
     }
-    return results;
+    if (!matched) {
+      throw { msg: "Invalid syntax", offset: offset, s: s };
+    }
+  }
+  return results;
 };
 
 const consIter = (head, tail) => {
-    let seen_head = false;
-    return {
-	next: () => {
-	    if (!seen_head) {
-		seen_head = true;
-		return { done: false, value: head };
-	    } else {
-		return tail.next();
-	    }
-	}
-    };
+  let seen_head = false;
+  return {
+    next: () => {
+      if (!seen_head) {
+        seen_head = true;
+        return { done: false, value: head };
+      } else {
+        return tail.next();
+      }
+    }
+  };
 }
 
 const compileListForm = (tokens) => {
-    let list = [];
-    while (true) {
-	const next = tokens.next();
-	if (next.done) {
-	    throw { msg: "Invalid list form: did not terminate" };
-	}
-	const [name, value] = next.value;
-	switch (name) {
-	case "RPAREN": return list;
-	default:
-	    list.push(compileForm(consIter(next.value, tokens)));
-	}
+  let list = [];
+  while (true) {
+    const next = tokens.next();
+    if (next.done) {
+      throw { msg: "Invalid list form: did not terminate" };
     }
+    const [name, value] = next.value;
+    switch (name) {
+    case "RPAREN": return list;
+    default:
+      list.push(compileForm(consIter(next.value, tokens)));
+    }
+  }
 };
 
 const compileForm = (tokens) => {
-    while (true) {
-	const next = tokens.next();
-	if (next.done) {
-	    throw { msg: "No tokens to compile for form" };
-	}
-	const [name, value] = next.value;
-	switch (name) {
-	case "INTEGER": return Number(value);
-	case "BOOLEAN": return value === "true";
-	case "NIL": return null;
-	case "KEYWORD": return { type: name, name: value };
-	case "SYMBOL": return { type: name, name: value };
-	case "WHITESPACE": continue;
-	case "LPAREN": return compileListForm(tokens);
-	}
-	throw { msg: "Unexpected token", name, value };
+  while (true) {
+    const next = tokens.next();
+    if (next.done) {
+      throw { msg: "No tokens to compile for form" };
     }
+    const [name, value] = next.value;
+    switch (name) {
+    case "INTEGER": return Number(value);
+    case "BOOLEAN": return value === "true";
+    case "NIL": return null;
+    case "KEYWORD": return { type: name, name: value };
+    case "SYMBOL": return { type: name, name: value };
+    case "WHITESPACE": continue;
+    case "LPAREN": return compileListForm(tokens);
+    }
+    throw { msg: "Unexpected token", name, value };
+  }
 };
 
 const compile = (s) => {
-    const tokens = tokenize(s);
-    const iterator = tokens[Symbol.iterator]();
-    return compileForm(iterator);
+  const tokens = tokenize(s);
+  const iterator = tokens[Symbol.iterator]();
+  return compileForm(iterator);
 };
 
 class Context {
-    constructor(parent) {
-	this.parent = parent;
-	this.bindings = new Map();
+  constructor(parent) {
+    this.parent = parent;
+    this.bindings = new Map();
+  }
+  
+  define(symbol, value) {
+    this.bindings.set(symbol.name, value);
+    return null;
+  }
+  
+  defineAll(symbols, values) {
+    symbols.reduce((accum, symbol, i) => this.define(symbol, values[i]), null);
+  }
+  
+  isDefined(symbol) {
+    return this.bindings.has(symbol.name);
+  }
+  
+  resolve(symbol) {
+    if (this.isDefined(symbol)) {
+      return this.bindings.get(symbol.name);
     }
-
-    define(symbol, value) {
-	this.bindings.set(symbol.name, value);
-	return null;
+    if (this.parent != null) {
+      return this.parent.resolve(symbol);
     }
-
-    defineAll(symbols, values) {
-	symbols.reduce((accum, symbol, i) => this.define(symbol, values[i]), null);
-    }
-
-    isDefined(symbol) {
-	return this.bindings.has(symbol.name);
-    }
-
-    resolve(symbol) {
-	if (this.isDefined(symbol)) {
-	    return this.bindings.get(symbol.name);
-	}
-	if (this.parent != null) {
-	    return this.parent.resolve(symbol);
-	}
-	throw { msg: "Undefined symbol", symbol, bindings: this.bindings };
-    }
+    throw { msg: "Undefined symbol", symbol, bindings: this.bindings };
+  }
 }
 
 const buildFn = (f) => ({type: "FN", apply: f});
 
 const buildMacro = (args, body) => ({
-    type: "MACRO", args, body});
+  type: "MACRO", args, body});
 
 const truthy = (value) => {
-    switch (value) {
-    case false: return false;
-    case null: return false;
-    default: return true;
-    }
+  switch (value) {
+  case false: return false;
+  case null: return false;
+  default: return true;
+  }
 };
 
 const evalForm = (context, form) => {
-    if (Array.isArray(form)) {
-	const [first, ...args] = form;
-	if (first.type == "SYMBOL") {
-	    // special forms
-	    switch (first.name) {
-	    case "def": {
-		const [def_symbol, value] = args;
-		context.define(def_symbol, evalForm(context, value));
-		return null;
-	    }
-	    case "defmacro": {
-		const [def_symbol, macro_args, body] = args;
-		context.define(def_symbol, buildMacro(macro_args, body));
-		return null;
-	    }
-	    case "fn": {
-		const [fn_args, body] = args;
-		return buildFn((call_context, call_args) => {
-		    // TODO consider adding 'context' as the final fallback
-		    const apply_context = new Context(call_context);
-		    apply_context.defineAll(fn_args, call_args);
-		    return evalForm(apply_context, body);
-		});
-	    }
-	    }
-	}
-	const value = evalForm(context, first);
-	switch (value.type) {
-	case "FN":
-	    const fn_args = args.map((arg) => evalForm(context, arg));
-	    return value.apply(context, fn_args);
-	case "MACRO":
-	    throw { msg: "TODO", form };
-	default:
-	    throw { msg: "Invalid callable value", value, form };
-	}
-    } else if (form.type == "SYMBOL") {
-	return context.resolve(form);
-    } else {
-	return form;
+  if (Array.isArray(form)) {
+    const [first, ...args] = form;
+    if (first.type == "SYMBOL") {
+      // special forms
+      switch (first.name) {
+      case "def": {
+        const [def_symbol, value] = args;
+        context.define(def_symbol, evalForm(context, value));
+        return null;
+      }
+      case "defmacro": {
+        const [def_symbol, macro_args, body] = args;
+        context.define(def_symbol, buildMacro(macro_args, body));
+        return null;
+      }
+      case "fn": {
+        const [fn_args, body] = args;
+        return buildFn((call_context, call_args) => {
+          // TODO consider adding 'context' as the final fallback
+          const apply_context = new Context(call_context);
+          apply_context.defineAll(fn_args, call_args);
+          return evalForm(apply_context, body);
+        });
+      }
+      }
     }
+    const value = evalForm(context, first);
+    switch (value.type) {
+    case "FN":
+      const fn_args = args.map((arg) => evalForm(context, arg));
+      return value.apply(context, fn_args);
+    case "MACRO":
+      throw { msg: "TODO", form };
+    default:
+      throw { msg: "Invalid callable value", value, form };
+    }
+  } else if (form.type == "SYMBOL") {
+    return context.resolve(form);
+  } else {
+    return form;
+  }
 };
 
 const evalString = (context, s) => evalForm(context, compile(s));
 
 const buildContext = (root, bindings) => {
-    const context = root || new Context();
-    if (bindings != null) {
-	for (const [name, value] of bindings) {
-	    context.define({name}, value);
-	}
+  const context = root || new Context();
+  if (bindings != null) {
+    for (const [name, value] of bindings) {
+      context.define({name}, value);
     }
-    return context;
+  }
+  return context;
 };
 
 const standardBindings = new Map([
-    ["+", buildFn((context, args) => args.reduce((accum, value) => accum + value, 0))],
+  ["+", buildFn((context, args) => args.reduce((accum, value) => accum + value, 0))],
 ]);
 
 const buildStandardContext = (bindings) => {
-    return buildContext(buildContext(null, standardBindings), bindings);
+  return buildContext(buildContext(null, standardBindings), bindings);
 };
 
 /*
--- repl.test.js
+++ repl.test.js
@@ -3,22 +3,22 @@
 const repl = require('./repl');
 
 test('compile', () => {
-    expect(repl.compile('(+ 1 2)')).toStrictEqual([{type: "SYMBOL", name: "+"}, 1, 2]);
+  expect(repl.compile('(+ 1 2)')).toStrictEqual([{type: "SYMBOL", name: "+"}, 1, 2]);
 });
 
 test('eval', () => {
-    const context = repl.buildStandardContext(new Map([["foo", 2]]));
-    const forms =
-	  [['2', 2],
-	   ['foo', 2],
-	   ['(def foo 3)', null],
-	   ['foo', 3],
-	   ['(+ 1 2)', 3],
-	   ['(+ 1 (+ 1 1))', 3],
-	   ['(def inc (fn (x) (+ x 1)))', null],
-	   ['(inc 4)', 5]
-	  ];
-    for (const [form, expected] of forms) {
-	expect(repl.eval(context, form)).toStrictEqual(expected);
-    }
+  const context = repl.buildStandardContext(new Map([["foo", 2]]));
+  const forms = [
+    ['2', 2],
+    ['foo', 2],
+    ['(def foo 3)', null],
+    ['foo', 3],
+    ['(+ 1 2)', 3],
+    ['(+ 1 (+ 1 1))', 3],
+    ['(def inc (fn (x) (+ x 1)))', null],
+    ['(inc 4)', 5]
+  ];
+  for (const [form, expected] of forms) {
+    expect(repl.eval(context, form)).toStrictEqual(expected);
+  }
 });
