6ecfa1e7da7ab4d947def12ddac856b84f5fda08
Donald Ball <donald.ball@gmail.com>
2019-09-01 09:48:36 -0400

    Delint

--- repl.js
+++ repl.js
@@ -1,30 +1,29 @@
-'use strict';
-
+/* eslint-disable no-console */
 const readline = require('readline');
-const stream = require('stream');
 const core = require('./core');
 
-const context = core.buildStandardContext(new Map([["*1", undefined],
-                                                   ["*2", undefined],
-                                                   ["*3", undefined],
-                                                   ["*e", undefined]]));
+const context = core.buildStandardContext(new Map([
+  ['*1', undefined],
+  ['*2', undefined],
+  ['*3', undefined],
+  ['*e', undefined]]));
 
 const repl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: "j> "
+  prompt: 'j> ',
 });
 
 repl.on('line', (line) => {
   try {
     const value = core.eval(context, line);
     console.log(value);
-    context.define("*3", context.resolve("*2"));
-    context.define("*2", context.resolve("*1"));
-    context.define("*1", value);
-  } catch(error) {
+    context.define('*3', context.resolve('*2'));
+    context.define('*2', context.resolve('*1'));
+    context.define('*1', value);
+  } catch (error) {
     console.error(error);
-    context.define("*e", error);
+    context.define('*e', error);
   }
   repl.prompt();
 });
